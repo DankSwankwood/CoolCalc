468b4186d81b361f40901dfcc885c25b525091b8
Vigg0 <victor@flyvholm.dk>
2014-03-26 11:34:46 +0100

    Added new window for test

---
+++ CoolCalc/Window_test2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CoolCalc
+{
+	public partial class Window_test2 : Gtk.Window
+	{
+		public Window_test2 () : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
