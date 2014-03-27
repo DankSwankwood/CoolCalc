c646d21af66d43d9883f2f21904994d8e78d3179
Niclas Bach Nielsen <niclas@h82.aau1x.klient.sydhavn2.site.aau.dk>
2014-03-27 15:17:36 +0100

    Added new project called CoolCalc2 - due to errors in the original CoolCalc project

---
+++ CoolCalc/NewWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoolCalc
+{
+	public partial class NewWindow : Gtk.Window
+	{
+		public NewWindow () : 
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
