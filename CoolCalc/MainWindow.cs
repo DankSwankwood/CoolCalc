d9b60c46c57bdcf594ae7e25f803b34fed072bb3
Niclas Bach Nielsen <niclas@h82.aau1x.klient.sydhavn2.site.aau.dk>
2014-03-27 14:20:17 +0100

    Redesign - Added buttons, changed button number button names.
    Easier implementation - added recognisable number button names.
    Gives weird errors

--- CoolCalc/MainWindow.cs
+++ CoolCalc/MainWindow.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using Gtk;
 
 namespace CoolCalc
 {
 	public partial class MainWindow : Gtk.Window
 	{
-		public MainWindow () : 
-			base (Gtk.WindowType.Toplevel)
+		public MainWindow () : base (Gtk.WindowType.Toplevel)
 		{
-			//Added comment for test
-			this.Build ();
-			//this is cool
+
+			Build ();
+
 		}
 
-		protected void OnButton7Clicked (object sender, EventArgs e)
+		protected void OnBtnNumZeroClicked (object sender, EventArgs e)
 		{
+			Console.WriteLine ("Button zero is pressed!");
 			throw new NotImplementedException ();
 		}
 
@@ -26,6 +27,48 @@ namespace CoolCalc
 		{
 			throw new NotImplementedException ();
 		}
+
+		protected void OnBtnNum3Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		protected void OnBtnNum4Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		protected void OnBtnNum5Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		protected void OnBtnNum6Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		protected void OnBtnNum7Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		protected void OnBtnNum8Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		protected void OnBtnNum9Clicked (object sender, EventArgs e)
+		{
+			//OnTextview1InsertAtCursor += "9";
+			throw new NotImplementedException ();
+		}
+
+//		protected void OnTextview1InsertAtCursor (object o, Gtk.InsertAtCursorArgs args)
+//		{
+//
+//			throw new NotImplementedException ();
+//		}
 	}
 }
 
---
+++ CoolCalc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace CoolCalc
+{
+	class MainClass
+	{
+		public static void main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
+
