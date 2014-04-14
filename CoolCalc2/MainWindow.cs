fdb8da4b7f2a8462fcec6fc632b298121e0cfcd6
Niclas Bach Nielsen <niclas.bach@gmail.com>
2014-04-14 08:16:15 +0200

    Deleted NotImplementdExceptions

--- CoolCalc2/MainWindow.cs
+++ CoolCalc2/MainWindow.cs
@@ -430,12 +430,10 @@ public partial class MainWindow: Gtk.Window
 
 	protected void OnDegreebutActivated (object sender, EventArgs e)
 	{
-		throw new NotImplementedException ();
 	}
 
 	protected void OnRadianbutActivated (object sender, EventArgs e)
 	{
-		throw new NotImplementedException ();
 	}
 
 
